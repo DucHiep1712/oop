8b571a926ed12e5c1eb8011d94b5a8e81c1efb01
Hiep <duchiep17122002@gmail.com>
2021-07-10 16:08:17 +0700

    Added more specific comments

--- src/StudyOop.java
+++ src/StudyOop.java
@@ -4,10 +4,11 @@ class Animal {
     public void sound() {
         System.out.println("The sound that ");
     }
-    //Overloading
     public void intro() {
         System.out.println("This is an animal!");
     }
+    //Overloading
+    //Cùng 1 tên hàm nhưng truyền các kiểu biến khác nhau cho ra các kết quả khác nhau
     public void intro(int n) {
         System.out.println("There are " + n + " dogs!");
     }
@@ -16,20 +17,46 @@ class Animal {
     }
 }
 
-abstract class Human {//Abstraction
+//Tạo class con
+class Dog extends Animal {//Inheritance(Tính thừa kế)
+    //Cho phép chia sẻ dữ liệu và phương thức cho các class con
+    public void sound() {
+        System.out.println("dogs make is Woof Woof");
+    }
+    //Overriding (Ghi đè phương thức)
+    //Việc kế thừa class cha giúp ta có thể thay đổi kết quả trả về của hàm của class con
+    public void intro() {
+        System.out.println("This is a dog!");
+    }
+}
+
+class Cat extends Animal {
+    public void sound() {
+        System.out.println("cats make is Meow Meow");
+    }
+}
+
+//Tạo class abstract
+abstract class Human {//Abstraction (Tính trừu tượng)
+    //Hàm abstract -> Không có thân
+    //Nên sử dụng abstract khi muốn chia sẻ code giữa một vài class có liên hệ thân thiết
     public abstract void work();
+    //Hàm bình thường
     public void sleep() {
         System.out.println("ZzZ");
     }
 }
 
+//class kế thừa
 class Teacher extends Human {
+    //Thân hàm abstract chỉ được định nghĩa cụ thể trong class kế thừa
     public void work() {
         System.out.println("Teaching");
     }
 }
 
 class Children extends Human {
+    //Thân hàm abstract nhận các kết quả khác nhau tùy vào cách sử dụng
     public void work() {
         for (int i = 0; i <= 4; i++) {
             System.out.println("The child is counting " + i + "\n");
@@ -37,10 +64,12 @@ class Children extends Human {
     }
 }
 
-class Student {//Encapsulation
+class Student {//Encapsulation (Tính đóng gói)
     private String name;
     private String idNum;
     private int age;
+    //Việc khai báo private khiến user không có quyền truy cập vào biến
+    //Được sử dụng khi cần giấu dữ liệu
     public int getAge() {
         return age;
     }
@@ -50,6 +79,7 @@ class Student {//Encapsulation
     public String getIdNum() {
         return idNum;
     }
+    //Chỉ có thể truy cập dữ liệu gián tiếp qua các hàm
     public void setAge(int newAge) {
         age = newAge;
     }
@@ -58,22 +88,6 @@ class Student {//Encapsulation
     }
     public void setIdNum( String newId) {
         idNum = newId;
-    }
-}
-
-class Dog extends Animal {//Inheritance(Tính thừa kế)
-    public void sound() {
-        System.out.println("dogs make is Woof Woof");
-    }
-    //Overriding
-    public void intro() {
-        System.out.println("This is a dog!");
-    }
-}
-
-class Cat extends Animal {
-    public void sound() {
-        System.out.println("cats make is Meow Meow");
     }
 }
 
